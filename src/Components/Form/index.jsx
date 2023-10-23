a8ba8bda70f8a686ea17944eadec939645b45789
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-23 14:16:40 -0700

    updated form to function based

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,37 +1,54 @@
-import React from 'react';
-
+import { useState } from 'react';
 import './Form.scss';
 
-class Form extends React.Component {
+function Form(props) {
+  const [formData, setFormData] = useState({
+    method: 'GET',
+    url: 'https://pokeapi.co/api/v2/pokemon',
+  });
 
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
-    };
-    this.props.handleApiCall(formData);
-  }
+    props.handleApiCall(formData);
+  };
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
-  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>URL: </span>
+          <input name='url' type='text' />
+          <button type='submit'>GO!</button>
+        </label>
+        <label className='methods'>
+          <span id='get'>GET</span>
+          <span id='post'>POST</span>
+          <span id='put'>PUT</span>
+          <span id='delete'>DELETE</span>
+        </label>
+      </form>
+    </>
+  );
 }
 
 export default Form;
+
+// class Form extends React.Component {
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const formData = {
+//       method:'GET',
+//       url: 'https://pokeapi.co/api/v2/pokemon',
+//     };
+//     this.props.handleApiCall(formData);
+//   }
+
+//   render() {
+//     return (
+
+//     );
+//   }
+// }
+
+// export default Form;
