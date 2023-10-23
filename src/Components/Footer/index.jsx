8a79bcb605384c35ef2a5783ca8a1d3bf5465cfc
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-23 14:06:46 -0700

    updated Footer to class based

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,5 @@
-import React from 'react';
-
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+function Footer() {
+  return <footer>&copy; 2023</footer>;
 }
 
 export default Footer;
